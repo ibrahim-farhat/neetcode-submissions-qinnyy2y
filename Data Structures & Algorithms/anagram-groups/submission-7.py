05a82fed7e0586f9b6efcd527f44cb9c6f0ee77e
Ibrahim Tarek <51857058+ibrahim-farhat@users.noreply.github.com>
2026-04-07 10:56:09 +0200

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # sort all of them
+        # map => each sorted string is a key
+        # traverse accross all of them, if it is in the map, put the index in within it, else make another key
+        # return the required list
+
+        anagram_map = defaultdict(list)
+
+        # with sorting solution:
+        # for word in strs:
+        #     key = ''.join(sorted(word))
+        #     anagram_map[key].append(word)
+        
+
+        # without sorting:
+        for word in strs:
+            word_counts = [0] * 26
+            for ch in word:
+                word_counts[ord(ch) - ord('a')] += 1
+            
+            anagram_map[tuple(word_counts)].append(word)
+
+        return list(anagram_map.values())
